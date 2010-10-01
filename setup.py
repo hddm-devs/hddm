72441bb2baccc530e2b65b860709e1a0b8a628d0
Thomas Wiecki <thomas.wiecki@gmail.com>
2010-10-01 20:27:49 +0000

    Added numpy header tracking to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
-
+import numpy as np
 
 setup(
     name="HDDM",
@@ -15,6 +15,7 @@ setup(
     description="HDDM is a python module that implements Hierarchical Bayesian estimation of Drift Diffusion Models.",
     install_requires=['NumPy >=1.3.0', 'PyMC >=2.0'],
     setup_requires=['NumPy >=1.3.0', 'PyMC >=2.0', 'cython'],
+    include_dirs = [np.get_include()],    
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension("wfpt", ["src/wfpt.pyx"]),
                    Extension("lba", ["src/lba.pyx"])]
