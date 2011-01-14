8c9f10452eb2bc02b79316d9549028b5ce3d0616
Thomas Wiecki <thomas.wiecki@gmail.com>
2011-01-14 13:51:25 +0000

    Minor.

--- hddm/make_wfpt64.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cp wfpt.pyx wfpt64.pyx
-cython wfpt64.pyx
-gcc -c wfpt64.c -fPIC -I/usr/include/python2.6 -I/usr/lib/python2.6/dist-packages/numpy/core/include
-gcc -shared -o wfpt64.so wfpt64.o -lblas -lm -lpython2.6 -O3 -fPIC -fno-strict-aliasing -fwrapv -pthread
