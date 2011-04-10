4de51d76a78411eff1a7755a0a4f0a6ac2854c70
Imri Sofer <imrisofer@gmail.com>
2011-04-09 20:19:07 -0400

    minor bug in wfpt.pdf_sign

--- src/wfpt.pyx
+++ src/wfpt.pyx
@@ -182,7 +182,7 @@ cpdef double pdf_diff(double x, double v, double a, double z, double err, char d
 cpdef double pdf_sign(double x, double v, double a, double z, double t, double err, int logp=0):
     """Wiener likelihood function for two response types. Lower bound
     responses have negative t, upper boundary response have positive t"""
-    if a<z or z<0 or z>1 or a<0:
+    if z<0 or z>1 or a<0:
         if logp==1:
             return -np.Inf
         else:
