dbdb0c8e0cab48aea93c7fb409cf24935bd2a63f
Imri Sofer <imrisofer@gmail.com>
2011-04-28 18:48:31 -0400

    added check_correl(ations) and rewritten the functions to work on general mcmc_models

--- hddm/diag.py
+++ hddm/diag.py
@@ -15,14 +15,13 @@ def check_model(model, params_true, assert_=False, conf_interval = 95):
 
     # Check for proper chain convergence
     #check_geweke(model, assert_=True)
-    print model.summary()
     # Test for correct parameter estimation
     fail = False
-    for param in model.param_names:
-        est = model.params_est[param]
-        est_std = model.params_est_std[param]
-        truth = params_true[param]
-        trace = model.group_params[param].trace()[:]
+    for node in model.stochastics:
+        trace = node.trace()[:]
+        est = np.mean(trace)
+        name = node.__name__
+        truth = params_true[name]
         lb = (50 - conf_interval/2.)
         lb_score = scoreatpercentile(trace, lb)
         ub = (50 + conf_interval/2.)
@@ -33,10 +32,10 @@ def check_model(model, params_true, assert_=False, conf_interval = 95):
 
 
         print "%s: Truth: %f, Estimated: %f, lb: %f, ub: %f,  fell in: %f" % \
-        (param, truth, est, lb_score, ub_score, fell)
+        (name, truth, est, lb_score, ub_score, fell)
         if (fell < lb) or (fell > ub):
             fail = True
-            print "the true value of %s is outsize of the confidence interval !*!*!*!*!*!*!" % param
+            print "the true value of %s is outsize of the confidence interval !*!*!*!*!*!*!" % name
         
     if assert_:
         assert (fail==False)
@@ -47,13 +46,14 @@ def check_model(model, params_true, assert_=False, conf_interval = 95):
 def check_rejection(model, assert_ = True):    
     """ check if the rejection ratio is not too high"""
     
-    for param in model.group_params:
-        trace = model.group_params[param].trace()[:]
+    for node in model.stochastics:
+        name = node.__name__
+        trace = node.trace()[:]
         rej =  np.sum(np.diff(trace)==0)
         rej_ratio = rej*1.0/len(trace)
-        print "rejection ratio for %s: %f" %(param, rej_ratio)
+        print "rejection ratio for %s: %f" %(name, rej_ratio)
         if (rej_ratio < 0.5) or (rej_ratio > 0.8):
-            msg = "%s still need to be tuned" % param
+            msg = "%s still need to be tuned" % name
             if assert_:
                 assert 1==0, msg
             else:
@@ -74,8 +74,8 @@ def rand_simple_params():
 
 def test_simple(nTimes=20):
     thin = 1
-    samples = 10000
-    burn = 10000
+    samples = 1000
+    burn = 1000
     n_iter = burn + samples*thin
     n_data = 300
     for i_time in range(nTimes):
@@ -83,8 +83,25 @@ def test_simple(nTimes=20):
         data,temp = hddm.generate.gen_rand_data(n_data, params)
         model = hddm.model.HDDM(data, no_bias=False)
         model.mcmc(sample=False);
-        model.mcmc_model.sample(n_iter, burn=burn, thin=thin)
-        [model.mcmc_model.use_step_method(pm.Metropolis, x,proposal_sd=0.001) for x in model.group_params.values()]
-        model._gen_stats()
+        model = model.mcmc_model
+        [model.use_step_method(pm.Metropolis, x,proposal_sd=0.1) for x in model.stochastics]
+        model.sample(n_iter, burn=burn, thin=thin)
         check_model(model, params, assert_=False)
         check_rejection(model, assert_ = False)
+        check_correl(model)
+
+def check_correl(model):
+    nodes = model.stochastics
+    threshold = 0.05
+    for node in nodes:
+        t_lag = np.inf
+        for lag in range(1,101):
+            corr = pm.diagnostics.autocorr(node.trace()[:], lag)
+            if corr <= threshold:
+                t_lag=  lag
+                break
+        if t_lag < np.inf:
+            print "%s: correlation drop under %f after %d steps" %(node.__name__ , threshold, t_lag)
+        else:
+            print "%s: correlation don't drop under %f!!!!" %(node.__name__ , threshold)
+        
