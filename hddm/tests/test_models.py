2461b125ec3d8a81c1821e837ee795d352ff7da8
Imri Sofer <imrisofer@gmail.com>
2011-04-28 10:35:12 -0400

    - changed check_model in test_models to check the precentile of the parameters - added a more specialized test for accuracy ( TestAcc )

--- hddm/tests/test_models.py
+++ hddm/tests/test_models.py
@@ -19,6 +19,8 @@ import hddm
 
 from hddm.likelihoods import *
 from hddm.generate import *
+from scipy.stats import scoreatpercentile
+from numpy.random import rand
 
 from nose import SkipTest
 
@@ -74,7 +76,7 @@ def diff_model(param, subj=True, num_subjs=10, change=.5, samples=10000):
     print model.summary()
     return model
 
-def check_model(model, params_true, assert_=True, equal_to_digits=1, err_bound=0.01):
+def check_model(model, params_true, assert_=True, conf_interval = 95):
     """calculate the posterior estimate error if hidden parameters are known (e.g. when simulating data)."""
     err=[]
 
@@ -82,22 +84,75 @@ def check_model(model, params_true, assert_=True, equal_to_digits=1, err_bound=0
     #check_geweke(model, assert_=True)
     print model.summary()
     # Test for correct parameter estimation
+    fail = False
     for param in model.param_names:
         est = model.params_est[param]
         est_std = model.params_est_std[param]
         truth = params_true[param]
         err.append((est - truth)**2)
+        trace = model.group_params[param].trace()[:]
+        lb = (50 - conf_interval/2.)
+        lb_score = scoreatpercentile(trace, lb)
+        ub = (50 + conf_interval/2.)
+        ub_score = scoreatpercentile(trace, ub)
+        fell = np.sum(truth > trace)*1./len(trace) * 100
 
-        print "%s: Truth: %f, Estimated: %f, Error: %f, Std: %f" % (param, truth, est, err[-1], est_std)
 
-        if assert_:
-            np.testing.assert_approx_equal(est, truth, equal_to_digits)
-            assert err[-1] < err_bound
+        print "%s: Truth: %f, Estimated: %f, lb: %f, ub: %f,  fell in: %f" % \
+        (param, truth, est, lb_score, ub_score, fell)
+        if (fell <= lb) or (fell >= ub):
+            fail = True
+            print "the true value of %s is not in the confidence interval" % param
+        
+    if assert_:
+        assert (fail==False) 
 
-    print "Summed error: %f" % np.sum(err)
-    return np.sum(err)
+   
+
+def check_rejection(model, rejection_precentage = 0.2, assert_ = True):    
+    """ check if the rejection ratio is not too high"""
+    
+    for param in model.group_params:
+        trace = model.group_params[param].trace()[:]
+        rej =  np.sum(np.diff(trace)==0)
+        rej_ratio = rej*1.0/len(trace)
+        if (rej_ratio) > rejection_precentage:
+            msg = "trace has too many rejections (%f)" % rej_ratio
+            if assert_:
+                assert 1==0, msg
+            else:
+                print msg
+            
+
+class TestAcc(unittest.TestCase):
+    
+    
+    def __init__(self, *args, **kwargs):
+        super(TestAcc, self).__init__(*args, **kwargs)     
+        self.samples = 10000
+        self.burn = 5000
+
+    def rand_simple_params(self):
+        params = {}
+        params['V'] = 0    
+        params['Z'] = 0
+        params['T'] = 0
+        params['v'] = (rand()-.5)*4
+        params['t'] = rand()*.5+(params['T']/2)
+        params['a'] = 1.5+rand()
+        params['z'] = .4+rand()*0.2
+        return params
 
     
+    def test_basic(self, nTimes=20):
+        for i_time in range(nTimes):
+            params = self.rand_simple_params()
+            data,temp = hddm.generate.gen_rand_data(300, params)
+            model = hddm.model.HDDM(data, no_bias=False)
+            model.mcmc(samples=self.samples, burn=self.burn)
+            check_model(model, params, assert_=False)
+            check_rejection(model, rejection_precentage = 0.001, assert_ = False)
+        
 class TestMulti(unittest.TestCase):
     def runTest(self):
         pass
