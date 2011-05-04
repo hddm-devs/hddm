* Fixed integration boundary for switch pdf * Fixed relative z conversion in density * Fixed histogram normalization in demo.py (drifts that did not cross a boundary were discarded, now they are used during histogram normalization) * Added gsl python wrapper.

hddm/demo.py
@@ -74,6 +74,7 @@ class DDM(HasTraits):
     params_dict = Property(Dict)
 
     histo = Property(Array, depends_on=['params'])
+    non_crossings = Property(Array, depends_on=['params'])
 
     # Total time.
     T = Float(5.0)
@@ -108,12 +109,14 @@ class DDM(HasTraits):
         return hddm.generate.find_thresholds(self.drifts, self.a)
 
     @cached_property
-    def _get_rts(self):
-        return hddm.generate.find_thresholds(self.drifts, self.a)
+    def _get_non_crossings(self):
+        return hddm.generate.find_thresholds(self.drifts, self.a, return_non_crossings=True)[1]
     
     @cached_property
     def _get_histo(self):
-        return hddm.utils.histogram(self.rts/self.dt, bins=2*self.bins, range=(-self.T,self.T), density=True)[0]
+        n, bins = hddm.utils.histogram(self.rts/self.dt, bins=2*self.bins, range=(-self.T,self.T))
+        db = np.array(np.diff(bins), float)
+        return n/db/(n.sum()+self.non_crossings)
     
     def _get_params(self):
         return np.array([self.a, self.v, self.ter, self.sz, self.sv, self.ster])
@@ -246,7 +249,7 @@ class DDMPlot(HasTraits):
                                              Z=self.ddm.sz,
                                              t=self.ddm.ter,
                                              T=self.ddm.ster,
-                                             a=self.ddm.a, err=.0001, reps=100)
+                                             a=self.ddm.a, err=1e-4, reps=100)
 
     @timer
     def _get_simple(self):
@@ -254,7 +257,7 @@ class DDMPlot(HasTraits):
                                   self.ddm.v,
                                   self.ddm.a,
                                   self.ddm.z,
-                                  self.ddm.ter, .001)
+                                  self.ddm.ter, 1e-4)
         return pdf
 
     @timer

hddm/generate.py
@@ -98,7 +98,7 @@ def simulate_drifts(params, samples, steps, T, intra_sv=1.):
 
     return drifts
 
-def find_thresholds(drifts, a):
+def find_thresholds(drifts, a, return_non_crossings=False):
     def interpolate_thresh(cross, boundary):
         # Interpolate to find crossing
         x1 = cross-1
@@ -114,6 +114,7 @@ def find_thresholds(drifts, a):
     # Find lines over the threshold
     rts_upper = []
     rts_lower = []
+    non_crossings = 0
     # Go through every drift individually
     for drift in drifts:
         # Check if upper bound was reached, and where
@@ -130,7 +131,7 @@ def find_thresholds(drifts, a):
 
         if upper == lower == np.Inf:
             # Threshold never crossed
-            continue
+            non_crossings += 1
 
         # Determine which one hit the threshold before
         if upper < lower:
@@ -144,7 +145,10 @@ def find_thresholds(drifts, a):
     rts_lower *= -1
     rts = np.concatenate((rts_upper, rts_lower))
 
-    return rts
+    if return_non_crossings:
+        return rts, non_crossings
+    else:
+        return rts
 
 def _gen_rts_fastdm(v=0, sv=0, z=0.5, sz=0, a=1, ter=0.3, ster=0, samples=500, fname=None, structured=True):
     """Generate simulated RTs with fixed parameters."""