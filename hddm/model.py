4394888e16f48a29fe958475088094b216d6749d
Thomas Wiecki <thomas.wiecki@gmail.com>
2011-07-07 14:03:46 -0400

    Cleaned up and documented code in model.py.

--- hddm/model.py
+++ hddm/model.py
@@ -13,13 +13,83 @@ import kabuki
 from kabuki.hierarchical import Parameter
 
 class Base(kabuki.Hierarchical):
+    """HDDM Base Class. Implements the hierarchical Ratcliff
+    drift-diffusion model using the Navarro & Fuss likelihood and
+    numerical integration over the variability parameters.
+
+    :Arguments:
+        data <numpy.recarray>: Input data with a row for each trial.
+            Must contain the following columns:
+              * 'rt': Reaction time of trial in seconds.
+              * 'response': Binary response (e.g. 0->error, 1->correct)
+            May contain:
+              * 'subj_idx': A unique ID (int) of the subject.
+              * Other user-defined columns that can be used in depends_on
+                keyword.
+
+    :Keyword arguments:
+        include <tuple=()>: Optional variability parameters to include.
+            Can be any combination of 'V', 'Z' and 'T'. Passing the string
+            'all' will include all three.
+            
+            :Note: Including 'Z' and/or 'T' will increase run time
+            significantly!
+
+        is_group_model <bool>: If True, this results in a hierarchical
+            model with separate parameter distributions for each
+            subject. The subject parameter distributions are
+            themselves distributed according to a group parameter
+            distribution.
+
+            If not provided, this parameter is set to True if data
+            provides a column 'subj_idx' and False otherwise.
+        
+        depends_on <dict>: Specifies which parameter depends on data
+            of a column in data. For each unique element in that
+            column, a separate set of parameter distributions will be
+            created and applied. Multiple columns can be specified in
+            a sequential container (e.g. list)
+
+            :Example: 
+
+            depends_on={'param1':['column1']}
+    
+            Suppose column1 has the elements 'element1' and
+            'element2', then parameters 'param1('element1',)' and
+            'param1('element2',)' will be created and the
+            corresponding parameter distribution and data will be
+            provided to the user-specified method get_liklihood().
+
+        bias <bool=False>: Whether to allow a bias to be
+            estimated. This is normally used when the responses represent
+            left/right and subjects could develop a bias towards
+            responding right. This is normally never done, however, when
+            the 'response' column codes correct/error.
+
+        trace_subjs <bool=True>: Save trace for subjs (needed for many
+             statistics so probably a good idea.)
+
+        plot_tau <bool=False>: Plot group variability parameters
+             (i.e. variance of Normal distribution.)
+
+        wiener_params <dict>: Parameters for wfpt evaluation and
+             numerical integration.
+             
+             :Parameters: 
+                 err: Error bound for wfpt (default 1e-4)
+                 nT: Maximum depth for numerical integration 
+                     for T (default 2)
+                 nZ: Maximum depth for numerical integration 
+                     for Z (default 2)
+                 use_adaptive: Whether to use adaptive numerical 
+                     integration (default True)
+                 simps_err: Error bound for Simpson integration
+                     (default 1e-3)
+
     """
     
-    """
-    
-    def __init__(self, data, trace_subjs=True, no_bias=True, 
-                 init=False, include=(), wiener_params = None,
-                 init_values=None, **kwargs):
+    def __init__(self, data, trace_subjs=True, bias=False,
+                 include=(), wiener_params = None, **kwargs):
         
         # Flip sign for lower boundary RTs
         self.data = hddm.utils.flip_errors(data)
@@ -32,46 +102,47 @@ class Base(kabuki.Hierarchical):
             else:
                 [include.add(param) for param in include]
 
-        self.no_bias = no_bias
-        
-        if not self.no_bias:
+        if bias:
             include.add('z')
 
-        if init:
-            raise NotImplementedError, "TODO"
-            # Compute ranges based on EZ method
-            param_ranges = hddm.utils.EZ_param_ranges(self.data)
-            # Overwrite set parameters
-            for param,value in param_ranges.iteritems():
-                self.param_ranges[param] = value
-            self.init_params = hddm.utils.EZ_subjs(self.data)
-        
-        if init_values is not None:
-            raise NotImplementedError, "TODO"
-
-        if wiener_params == None:
-            self.wiener_params = {'err': 1e-4, 'nT':2, 'nZ':2, 'use_adaptive':1, 'simps_err':1e-3}
-        else:          
+        if wiener_params is None:
+            self.wiener_params = {'err': 1e-4, 'nT':2, 'nZ':2,
+                                  'use_adaptive':1,
+                                  'simps_err':1e-3}
+            self.wfpt = hddm.likelihoods.WienerFullIntrp
+        else:
             self.wiener_params = wiener_params
-        
+            wp = self.wiener_params
+            self.wfpt = hddm.likelihoods.general_WienerFullIntrp_variable(err=wp['err'], nT=wp['nT'], nZ=wp['nZ'], use_adaptive=wp['use_adaptive'], simps_err=wp['simps_err'])
+
         self.params = self.get_params()
 
         super(hddm.model.Base, self).__init__(data, include=include, **kwargs)
 
     def get_params(self):
+        """Returns list of model parameters.
+        """
         params = [Parameter('a', True, lower=.5, upper=4.5),
                   Parameter('v', True, lower=-10., upper=10.), 
                   Parameter('t', True, lower=.1, upper=2., init=.1),
-                  Parameter('z', True, lower=0., upper=1., init=.5, default=.5, optional=True),
-                  Parameter('V', True, lower=0., upper=6., default=0, optional=True),
-                  Parameter('Z', True, lower=0., upper=1., init=.1, default=0, optional=True),
-                  Parameter('T', True, lower=0., upper=2., init=.1, default=0, optional=True),
+                  Parameter('z', True, lower=0., upper=1., init=.5, 
+                            default=.5, optional=True),
+                  Parameter('V', True, lower=0., upper=6., default=0, 
+                            optional=True),
+                  Parameter('Z', True, lower=0., upper=1., init=.1, 
+                            default=0, optional=True),
+                  Parameter('T', True, lower=0., upper=2., init=.1, 
+                            default=0, optional=True),
                   Parameter('wfpt', False)]
         
         return params
     
     def get_root_node(self, param):
-        """Create and return a prior distribution for [param].
+        """Create and return a uniform prior distribution for root
+        parameter [param].
+
+        This is used for the group distributions.
+
         """
         return pm.Uniform(param.full_name,
                           lower=param.lower,
@@ -80,9 +151,27 @@ class Base(kabuki.Hierarchical):
                           verbose=param.verbose)
 
     def get_tau_node(self, param):
+        """Create and return a Uniform prior distribution for the
+        variability parameter [param].
+        
+        Note, that we chose a Uniform distribution rather than the
+        more common Gamma (see Gelman 2006: "Prior distributions for
+        variance parameters in hierarchical models").
+
+        This is used for the variability fo the group distribution.
+
+        """
         return pm.Uniform(param.full_name, lower=0., upper=1., value=.1, plot=self.plot_tau)
 
     def get_child_node(self, param):
+        """Create and return a Normal (in case of an effect or
+        drift-parameter) or Truncated Normal (otherwise) distribution
+        for [param] centered around param.root with standard deviation
+        param.tau and initialization value param.init.
+
+        This is used for the individual subject distributions.
+
+        """
         if param.name.startswith('e') or param.name.startswith('v'):
             return pm.Normal(param.full_name,
                              mu=param.root,
@@ -100,24 +189,24 @@ class Base(kabuki.Hierarchical):
                                       value=param.init)
     
     def get_rootless_child(self, param, params):
+        """Create and return the wiener likelihood distribution
+        supplied in [param]. 
+
+        [params] is a dictionary of all parameters on which the data
+        depends on (i.e. condition and subject).
+
+        """
         if param.name == 'wfpt':
-            if self.wiener_params is not None:
-                wp = self.wiener_params
-                WienerFullIntrp = hddm.likelihoods.general_WienerFullIntrp_variable(err=wp['err'], nT=wp['nT'], nZ=wp['nZ'],
-                                                                                    use_adaptive=wp['use_adaptive'], simps_err=wp['simps_err'])
-            else:
-                WienerFullIntrp =  hddm.likelihoods.WienerFullIntrp
-            
-            return WienerFullIntrp(param.full_name,
-                                   value=param.data['rt'].flatten(),
-                                   v = params['v'],
-                                   a = params['a'],
-                                   z = self.get_node('z',params),
-                                   t = params['t'],
-                                   Z = self.get_node('Z',params),
-                                   T = self.get_node('T',params),
-                                   V = self.get_node('V',params),
-                                   observed=True)
+            return self.wfpt(param.full_name,
+                             value=param.data['rt'].flatten(),
+                             v = params['v'],
+                             a = params['a'],
+                             z = self.get_node('z',params),
+                             t = params['t'],
+                             Z = self.get_node('Z',params),
+                             T = self.get_node('T',params),
+                             V = self.get_node('V',params),
+                             observed=True)
 
         else:
             raise KeyError, "Rootless parameter named %s not found." % param.name
