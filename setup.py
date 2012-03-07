92fe50f5ae47842c8d058701218cf62c5f3695a9
Thomas Wiecki <thomas.wiecki@gmail.com>
2012-03-07 10:11:12 -0500

    Bumped version to 0.2RC1. Time to start testing.

--- setup.py
+++ setup.py
@@ -5,17 +5,16 @@ import os
 
 setup(
     name="HDDM",
-    version="0.2a",
+    version="0.2RC1",
     author="Thomas V. Wiecki, Imri Sofer, Michael J. Frank",
     author_email="thomas_wiecki@brown.edu",
     url="http://github.com/hddm-devs/hddm",
     packages=["hddm", "hddm.tests", "hddm.sandbox"],
     package_data={"hddm":["examples/*.csv", "examples/*.conf"]},
-    #package_dir={"hddm":"hddm/examples"},
     scripts=["scripts/hddm_fit.py", "scripts/hddm_demo.py"],
     description="HDDM is a python module that implements Hierarchical Bayesian estimation of Drift Diffusion Models.",
-    install_requires=['NumPy >=1.3.0', 'kabuki >= 0.2a', 'pymc'],
-    setup_requires=['NumPy >=1.3.0', 'kabuki >= 0.2a', 'pymc'],
+    install_requires=['NumPy >=1.3.0', 'kabuki >= 0.2', 'pymc'],
+    setup_requires=['NumPy >=1.3.0', 'kabuki >= 0.2', 'pymc'],
     include_dirs = [np.get_include()],
     classifiers=[
                 'Development Status :: 4 - Beta/Unstable',
@@ -26,6 +25,6 @@ setup(
                 'Programming Language :: Python',
                 'Topic :: Scientific/Engineering',
                  ],
-    ext_modules = [Extension("wfpt", ["src/wfpt.c"])]#, extra_compile_args=['-fopenmp'], extra_link_args=['-fopenmp'])]
+    ext_modules = [Extension("wfpt", ["src/wfpt.c"])]
 )
 
