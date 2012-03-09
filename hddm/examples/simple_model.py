c376dabde10f1615924e471ad8b927aaa9588912
Thomas Wiecki <thomas.wiecki@gmail.com>
2012-03-09 13:42:56 -0500

    CHG: Example model uses singe subject data now.

--- hddm/examples/simple_model.py
+++ hddm/examples/simple_model.py
@@ -1,13 +1,13 @@
 import hddm
 
 # Load data from csv file into a NumPy structured array
-data = hddm.load_csv('simple_subjs_difficulty.csv')
+data = hddm.load_csv('simple_difficulty.csv')
 
 # Create a HDDM model multi object
 model = hddm.HDDM(data, depends_on={'v':'difficulty'})
 
 # Create model and start MCMC sampling
-model.sample(1000, burn=500)
+model.sample(10000, burn=5000)
 
 # Print fitted parameters and other model statistics
 model.print_stats()
