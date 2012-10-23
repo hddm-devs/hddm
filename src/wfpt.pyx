da36c9750db80cd340fcf137f07ff73573b20536
Imri Sofer <imrisofer@gmail.com>
2012-10-22 21:07:50 -0400

    DEP: remove gen_cdf_using_fastdm

--- src/wfpt.pyx
+++ src/wfpt.pyx
@@ -177,25 +177,6 @@ def gen_cdf_using_pdf(double v, double sv, double a, double z, double sz, double
 
     return x, cdf_array
 
-def gen_cdf_using_fastdm(double v, double sv, double a, double z, double sz, double t, double st, double precision=3.,
-            int N=500, double time=5., np.ndarray[double, ndim=1] cdf_array=None):
-    """
-    generate cdf vector using fast-dm
-    """
-
-    if (sv < 0) or (a <=0 ) or (z < 0) or (z > 1) or (sz < 0) or (sz > 1) or (z+sz/2.>1) or \
-    (z-sz/2.<0) or (t-st/2.<0) or (t<0) or (st < 0):
-        raise ValueError("at least one of the parameters is out of the support")
-
-    if cdf_array is None:
-        cdf_array = np.empty(2*N+1, dtype=np.double)
-
-    cdef np.ndarray[double, ndim=1] x = np.linspace(-time, time, 2*N+1)
-
-    cdef double* cdf_ptr = cdf(v, sv, a, z, sz, t, st, precision, N, time, <double *> cdf_array.data)
-
-    return x, cdf_array
-
 def split_cdf(np.ndarray[double, ndim=1] x, np.ndarray[double, ndim=1] data):
 
     #get length of data
