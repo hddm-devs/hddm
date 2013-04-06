0a033fc2d46793bed5f3b5c963e7da26acb949c9
Thomas Wiecki <thomas.wiecki@gmail.com>
2013-04-05 17:37:17 -0700

    DOC: Bumped version to 0.5beta.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import numpy as np
 
 setup(
     name='HDDM',
-    version='0.4.1',
+    version='0.5beta',
     author='Thomas V. Wiecki, Imri Sofer, Michael J. Frank',
     author_email='thomas_wiecki@brown.edu',
     url='http://github.com/hddm-devs/hddm',
