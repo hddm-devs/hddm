86d26d69b47701a31b6572c35fb96fc7b2e8d98e
Thomas Wiecki <thomas.wiecki@gmail.com>
2013-10-07 21:25:16 -0400

    ENH: Added informative priors and outliers to LBA. Refactored.

--- hddm/models/hlba_truncated.py
+++ hddm/models/hlba_truncated.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 
 import numpy as np
 from numpy.random import rand, randn
+import pymc as pm
 
 import hddm
 import kabuki
@@ -10,10 +11,10 @@ from base import AccumulatorModel
 from kabuki.hierarchical import Knode
 from kabuki.utils import stochastic_from_dist
 
-def lba_like(value, t, A, b, s, v):
+def lba_like(value, t, A, b, s, v, p_outlier, w_outlier=.1):
     """LBA likelihood.
     """
-    return hddm.lba.lba_like(value['rt'], A, b, t, s, v, 0, logp=True, normalize_v=True)
+    return hddm.lba.lba_like(value['rt'], A, b, t, s, v, 0, normalize_v=True, p_outlier=p_outlier, w_outlier=w_outlier)
 
 def lba_random(t, A, b, s, v, size=1):
     """Generate RTs from LBA process.
@@ -48,28 +49,113 @@ def lba_random(t, A, b, s, v, size=1):
 lba_class = stochastic_from_dist(name='lba_like', logp=lba_like, random=lba_random)
 
 class HLBA(AccumulatorModel):
-    def _create_lba_knode(self, knodes):
-        lba_parents = OrderedDict()
-        lba_parents['t'] = knodes['t_bottom']
-        lba_parents['A'] = knodes['A_bottom']
-        lba_parents['b'] = knodes['b_bottom']
-        lba_parents['s'] = knodes['s_bottom']
-        lba_parents['v'] = knodes['v_bottom']
+    def __init__(self, *args, **kwargs):
+        self.informative = kwargs.get('informative', True)
+        self.std_depends = kwargs.get('std_depends', False)
+        self.slice_widths = {'b':1, 'A':.05, 'A_std': 1, 'v1': .5, 'v2': .5, 'p_outlier':1.}
+        self.p_outlier = kwargs.get('p_outlier', 0.)
+        if self.p_outlier is True:
+            self.include = ['p_outlier']
+        else:
+            self.include = []
 
-        return Knode(lba_class, 'lba', observed=True, col_name='rt', **lba_parents)
+        super(hddm.AccumulatorModel, self).__init__(*args, **kwargs)
 
-    def create_knodes(self):
-        """Returns list of model parameters.
+    def pre_sample(self, use_slice=True):
+        from kabuki import steps
+        for name, node_descr in self.iter_stochastics():
+            node = node_descr['node']
+            knode_name = node_descr['knode_name'].replace('_subj', '')
+            self.mc.use_step_method(steps.SliceStep, node, width=self.slice_widths.get(knode_name, 1),
+                                    left=0, maxiter=5000)
+
+    def _create_family_beta(self, name, value=.5, g_value=.5, g_mean=.5, g_certainty=2,
+                           var_alpha=1, var_beta=1, var_value=.1):
+        """Similar to create_family_normal() but beta for the subject
+        and group mean nodes. This is useful when the parameter space
+        is restricted from [0, 1].
+
+        See create_family_normal() help for more information.
+
         """
 
         knodes = OrderedDict()
-        knodes.update(self._create_family_trunc_normal('t', lower=0, upper=1, value=.01))
+        if self.is_group_model and name not in self.group_only_nodes:
+            g_mean = Knode(pm.Beta, name, alpha=g_mean*g_certainty, beta=(1-g_mean)*g_certainty,
+                      value=g_value, depends=self.depends[name])
+
+            g_certainty = Knode(pm.Gamma, '%s_certainty' % name,
+                                alpha=var_alpha, beta=var_beta, value=var_value)
+
+            alpha = Knode(pm.Deterministic, '%s_alpha' % name, eval=lambda mean, certainty: mean*certainty,
+                      mean=g_mean, certainty=g_certainty, plot=False, trace=False, hidden=True)
+
+            beta = Knode(pm.Deterministic, '%s_beta' % name, eval=lambda mean, certainty: (1-mean)*certainty,
+                      mean=g_mean, certainty=g_certainty, plot=False, trace=False, hidden=True)
+
+            subj = Knode(pm.Beta, '%s_subj'%name, alpha=alpha, beta=beta,
+                         value=value, depends=('subj_idx',),
+                         subj=True, plot=False)
+
+            knodes['%s'%name] = g_mean
+            knodes['%s_certainty'%name] = g_certainty
+            knodes['%s_alpha'%name] = alpha
+            knodes['%s_beta'%name] = beta
+            knodes['%s_bottom'%name] = subj
+
+        else:
+            g = Knode(pm.Beta, name, alpha=g_mean*g_certainty, beta=(1-g_mean)*g_certainty, value=value,
+                      depends=self.depends[name])
+
+            knodes['%s_bottom'%name] = g
+
+        return knodes
+
+    def _create_knodes_informative(self):
+        knodes = OrderedDict()
+        knodes.update(self._create_family_gamma_gamma_hnormal('t', g_mean=.4, g_std=0.2, value=0.001, std_std=1, std_value=0.2))
+        knodes.update(self._create_family_gamma_gamma_hnormal('b', g_mean=2, g_std=2, std_std=2, std_value=0.1, value=2))
+        knodes.update(self._create_family_gamma_gamma_hnormal('A', g_mean=.5, g_std=1, std_std=2, std_value=0.1, value=.2))
+        knodes.update(self._create_family_gamma_gamma_hnormal('s', g_mean=1, g_std=2, std_std=2, value=1.))
+        knodes.update(self._create_family_beta('v', value=.75, g_mean=.75, g_certainty=0.75**-2))
+        if 'p_outlier' in self.include:
+            knodes['p_outlier_bottom'] = Knode(pm.Beta, 'p_outlier', alpha=1, beta=15, value=0.01, depends=self.depends['p_outlier'])
+        #knodes.update(self._create_family_gamma_gamma_hnormal('v', g_mean=1, g_std=2, std_std=2, value=0.5))
+
+        return knodes
+
+    def _create_knodes_noninformative(self):
+        """Returns list of model parameters.
+        """
+        knodes = OrderedDict()
+        knodes.update(self._create_family_trunc_normal('t', lower=0, upper=1, value=.001))
         knodes.update(self._create_family_trunc_normal('A', lower=1e-3, upper=10, value=.2))
         knodes.update(self._create_family_trunc_normal('b', lower=1e-3, upper=10, value=1.5))
         knodes.update(self._create_family_trunc_normal('s', lower=0, upper=10, value=1.))
         knodes.update(self._create_family_trunc_normal('v', lower=0, upper=1, value=.5))
+        if 'p_outlier' in self.include:
+            knodes['p_outlier_bottom'] = Knode(pm.Beta, 'p_outlier', alpha=1, beta=15, value=0.01, depends=self.depends['p_outlier'])
 
-        knodes['wfpt'] = self._create_lba_knode(knodes)
+        return knodes
+
+    def _create_lba_knode(self, knodes):
+        lba_parents = OrderedDict()
+        lba_parents['t'] = knodes['t_bottom']
+        lba_parents['v'] = knodes['v_bottom']
+        lba_parents['A'] = knodes['A_bottom']
+        lba_parents['b'] = knodes['b_bottom']
+        lba_parents['s'] = knodes['s_bottom']
+        lba_parents['p_outlier'] = knodes['p_outlier_bottom'] if 'p_outlier' in self.include else self.p_outlier
+
+        return Knode(lba_class, 'lba', observed=True, col_name='rt', **lba_parents)
+
+    def create_knodes(self):
+        if self.informative:
+            knodes = self._create_knodes_informative()
+        else:
+            knodes = self._create_knodes_noninformative()
+
+        knodes['lba'] = self._create_lba_knode(knodes)
 
         return knodes.values()
 
--- src/lba.pyx
+++ src/lba.pyx
@@ -8,7 +8,7 @@
 from __future__ import division
 from copy import copy
 import numpy as np
-cimport numpy as cnp
+cimport numpy as np
 
 import scipy as sp
 import scipy.stats
@@ -18,8 +18,7 @@ cimport cython
 
 # Define data type
 DTYPE = np.double
-ctypedef cnp.double_t DTYPE_t
-cdef double PI = 3.1415926535897
+ctypedef np.double_t DTYPE_t
 
 cdef extern from "math.h":
     double sin(double)
@@ -56,13 +55,20 @@ cdef DTYPE_t fptpdf_single(DTYPE_t t, double z, double a, double driftrate, doub
 
     return (driftrate*(norm_cdf(azu)-norm_cdf(azumax)) + sddrift*(norm_pdf(azumax)-norm_pdf(azu)))/z
 
-def lba_like(cnp.ndarray[DTYPE_t, ndim=1] value, double z, double a, double ter, double sv, double v0, double v1, bint logp=False, bint normalize_v=False):
-    cdef cnp.ndarray[DTYPE_t, ndim=1] rt = (np.abs(value) - ter)
-    cdef unsigned int nresp = <unsigned int> value.shape[0]
-    cdef unsigned int i = 0
-    cdef cnp.ndarray[DTYPE_t, ndim=1] probs = np.empty_like(value)
+cdef inline bint p_outlier_in_range(double p_outlier): return (p_outlier >= 0) & (p_outlier <= 1)
+
+def lba_like(np.ndarray[DTYPE_t, ndim=1] value, double z, double a, double ter, double sv, double v0, double v1, bint normalize_v=False, double p_outlier=0, double w_outlier=0):
+    cdef np.ndarray[DTYPE_t, ndim=1] rt = (np.abs(value) - ter)
+    cdef Py_ssize_t size = value.shape[0]
+    cdef Py_ssize_t i = 0
+    cdef double sum_logp = 0
+    cdef double p = 0
+    cdef double wp_outlier = w_outlier * p_outlier
 
     assert sv >= 0, "sv must be larger than 0"
+
+    if not p_outlier_in_range(p_outlier):
+        return -np.inf
 
     if normalize_v == 1:
         v1 = 1 - v0
@@ -72,42 +78,18 @@ def lba_like(cnp.ndarray[DTYPE_t, ndim=1] value, double z, double a, double ter,
         return -np.Inf
 
     #print "z: %f, a: %f, ter: %i, v: %f, sv: %f" % (z, a, ter, v[0], sv)
-    for i from 0 <= i < nresp:
+    for i in range(size):
         if value[i] > 0:
-            probs[i] = (1-fptcdf_single(rt[i], z, a, v1, sv)) * \
-                       fptpdf_single(rt[i], z, a, v0, sv)
+            p = (1-fptcdf_single(fabs(value[i]), z, a, v1, sv)) * \
+                         fptpdf_single(fabs(value[i]), z, a, v0, sv)
         else:
-            probs[i] = (1-fptcdf_single(rt[i], z, a, v0, sv)) * \
-                       fptpdf_single(rt[i], z, a, v1, sv)
+            p = (1-fptcdf_single(fabs(value[i]), z, a, v0, sv)) * \
+                       fptpdf_single(fabs(value[i]), z, a, v1, sv)
 
-    if logp == 1:
-        return np.sum(np.log(probs))
-    else:
-        return probs
+        p = p * (1 - p_outlier) + wp_outlier
+        if p == 0:
+            return -np.inf
 
+        sum_logp += log(p)
 
-# Backed up if in the future multiple responses are to be supported
-def lba_like_multi(cnp.ndarray[DTYPE_t, ndim=1] value, cnp.ndarray[DTYPE_t, ndim=1] resps, double z, double a, double ter, cnp.ndarray[DTYPE_t, ndim=1] drift, double sv, unsigned int logp=0):
-    # Rescale parameters so as to fit in the same range as the DDM
-    cdef cnp.ndarray[DTYPE_t, ndim=1] rt = (np.abs(value) - ter)
-    cdef unsigned int nresp = value.shape[0]
-    cdef unsigned int i
-    cdef cnp.ndarray[DTYPE_t, ndim=1] probs = np.empty(nresp, dtype=DTYPE)
-
-    if a <= z:
-        #print "Starting point larger than threshold!"
-        return -np.Inf
-
-    #print "z: %f, a: %f, ter: %i, v: %f, sv: %f" % (z, a, ter, v[0], sv)
-    for i from 0 <= i < nresp:
-        if resps[i] == 1:
-            probs[i] = (1-fptcdf_single(rt[i], z, a, drift[1], sv)) * \
-                       fptpdf_single(rt[i], z, a, drift[0], sv)
-        else:
-            probs[i] = (1-fptcdf_single(rt[i], z, a, drift[0], sv)) * \
-                       fptpdf_single(rt[i], z, a, drift[1], sv)
-
-    if logp == 1:
-        return np.sum(np.log(probs))
-    else:
-        return probs
+    return sum_logp
