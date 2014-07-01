40eccf39d63e5554a89c7e2b78f09594ef570423
Thomas Wiecki <thomas.wiecki@gmail.com>
2014-07-01 20:24:10 +0200

    Bumped version to 0.5.4 and updated pymc.

--- hddm/__init__.py
+++ hddm/__init__.py
@@ -2,7 +2,7 @@
 
 __docformat__ = 'restructuredtext'
 
-__version__ = '0.5.4.dev'
+__version__ = '0.5.4'
 
 import likelihoods
 import generate
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import numpy as np
 
 setup(
     name='HDDM',
-    version='0.5.4.dev',
+    version='0.5.4',
     author='Thomas V. Wiecki, Imri Sofer, Michael J. Frank',
     author_email='thomas_wiecki@brown.edu',
     url='http://github.com/hddm-devs/hddm',
