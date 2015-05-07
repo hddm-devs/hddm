ab6b02276b75bd3d7a8ef966022d9aae227b5258
Thomas Wiecki <thomas.wiecki@gmail.com>
2015-05-07 20:52:40 +0200

    Fix windows conda build.

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,3 +1,3 @@
-%SYS_PYTHON% setup.py build --compiler=mingw32
-%SYS_PYTHON% setup.py install --prefix=%PREFIX%
+%PYTHON% setup.py build --compiler=mingw32
+%PYTHON% setup.py install
 if errorlevel 1 exit 1
