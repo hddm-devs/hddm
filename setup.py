d9c22914c85c9e93c5d8e95dae929c2a084a5ebb
Thomas Wiecki <thomas.wiecki@gmail.com>
2015-12-03 14:45:04 +0100

    Bumped version to 0.6.0.

--- setup.py
+++ setup.py
@@ -16,16 +16,16 @@ import numpy as np
 
 setup(
     name='HDDM',
-    version='0.5.5',
+    version='0.6.0',
     author='Thomas V. Wiecki, Imri Sofer, Michael J. Frank',
-    author_email='thomas_wiecki@brown.edu',
+    author_email='thomas.wiecki@gmail.com',
     url='http://github.com/hddm-devs/hddm',
     packages=['hddm', 'hddm.tests', 'hddm.models', 'hddm.examples'],
     package_data={'hddm':['examples/*.csv', 'examples/*.conf']},
     scripts=['scripts/hddm_demo.py'],
     description='HDDM is a python module that implements Hierarchical Bayesian estimation of Drift Diffusion Models.',
-    install_requires=['NumPy >=1.6.0', 'SciPy >= 0.6.0', 'kabuki >= 0.5.3', 'PyMC>=2.3.3', 'pandas >= 0.12.0', 'patsy'],
-    setup_requires=['NumPy >=1.6.0', 'SciPy >= 0.6.0', 'kabuki >= 0.5.3', 'PyMC>=2.3.3', 'pandas >= 0.12.0', 'patsy'],
+    install_requires=['NumPy >=1.6.0', 'SciPy >= 0.6.0', 'kabuki >= 0.6.0', 'PyMC>=2.3.3', 'pandas >= 0.12.0', 'patsy'],
+    setup_requires=['NumPy >=1.6.0', 'SciPy >= 0.6.0', 'kabuki >= 0.6.0', 'PyMC>=2.3.3', 'pandas >= 0.12.0', 'patsy'],
     include_dirs = [np.get_include()],
     classifiers=[
                 'Development Status :: 5 - Production/Stable',
