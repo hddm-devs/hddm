55154b0cc8a2f031bd655d2ffbd5782ebe62d0e5
madslupe <madslupe@gmail.com>
2018-09-26 15:01:47 -0400

    Update hddm_rl.py

--- hddm/models/hddm_rl.py
+++ hddm/models/hddm_rl.py
@@ -18,7 +18,7 @@ class HDDMrl(HDDM):
     """
     def __init__(self, *args, **kwargs):
         self.alpha = kwargs.pop('alpha', True)
-        self.dual_alpha = kwargs.pop('dual_alpha', True)
+        #self.dual_alpha = kwargs.pop('dual_alpha', True)
         self.wfpt_rl_class = WienerRL
 
         super(HDDMrl, self).__init__(*args, **kwargs)
@@ -35,7 +35,7 @@ class HDDMrl(HDDM):
                                                                     std_upper=10, 
                                                                     std_value=.1))
             
-        if self.dual_alpha:
+        if 'dual_alpha' in include:
             # Add second learning rate parameter
             knodes.update(self._create_family_normal('dual_alpha',
                                                                     value=0,
@@ -50,7 +50,8 @@ class HDDMrl(HDDM):
         wfpt_parents = super(HDDMrl, self)._create_wfpt_parents_dict(knodes)
 
         wfpt_parents['alpha'] = knodes['alpha_bottom'] 
-        wfpt_parents['dual_alpha'] = knodes['dual_alpha_bottom'] 
+        if 'dual_alpha' in include:
+            wfpt_parents['dual_alpha'] = knodes['dual_alpha_bottom'] 
         return wfpt_parents
 
     #use own wfpt_class, defined in the init
