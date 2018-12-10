a491e1c336f7612cb49c3e31c5b4070aab4f920f
madslupe <madslupe@gmail.com>
2018-12-10 10:01:33 -0500

    Update wfpt.pyx

--- src/wfpt.pyx
+++ src/wfpt.pyx
@@ -82,6 +82,7 @@ def wiener_like_rlddm(np.ndarray[double, ndim=1] x,
                       double p_outlier=0, double w_outlier=0):
     cdef Py_ssize_t size = x.shape[0]
     cdef Py_ssize_t i
+    cdef int s
     cdef double p
     cdef double sum_logp = 0
     cdef double wp_outlier = w_outlier * p_outlier
