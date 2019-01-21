da7a76dfb62200cf44d01d2507f43a3b08e4ad6c
madslupe <madslupe@gmail.com>
2019-01-21 16:07:06 -0500

    Update wfpt.pyx

--- src/wfpt.pyx
+++ src/wfpt.pyx
@@ -74,7 +74,7 @@ def wiener_like(np.ndarray[double, ndim=1] x, double v, double sv, double a, dou
 
 def wiener_like_rlddm(np.ndarray[double, ndim=1] x, 
                       np.ndarray[long, ndim=1] response,
-                      np.ndarray[long, ndim=1] feedback,
+                      np.ndarray[double, ndim=1] feedback,
                       np.ndarray[double, ndim=1] q,
                       np.ndarray[long, ndim=1] split_by,
                       long unique,
