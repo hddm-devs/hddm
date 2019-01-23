973ee80e00d4b89ef2be6d2585b84e5c2b3c45b0
madslupe <madslupe@gmail.com>
2019-01-23 10:52:03 -0500

    Update hddm_rl.py

--- hddm/models/hddm_rl.py
+++ hddm/models/hddm_rl.py
@@ -16,7 +16,7 @@ class HDDMrl(HDDM):
     """HDDM model that can be used for two-armed bandit tasks.
 
     """
-    def __init__(self,uncertainty=False, *args, **kwargs):
+    def __init__(self,uncertainty=False,q_up=0.5,q_low=0.5, *args, **kwargs):
         self.alpha = kwargs.pop('alpha', True)
         self.dual_alpha = kwargs.pop('dual_alpha', False)
         self.wfpt_rl_class = WienerRL
@@ -48,7 +48,7 @@ class HDDMrl(HDDM):
     def _create_wfpt_knode(self, knodes):
         wfpt_parents = self._create_wfpt_parents_dict(knodes)
         return Knode(self.wfpt_rl_class, 'wfpt',
-                                   observed=True, col_name=['split_by','feedback', 'response', 'rt','q'],
+                                   observed=True, col_name=['split_by','feedback', 'response', 'rt'],
                                    **wfpt_parents)
 
 def wienerRL_like(x, v, alpha,dual_alpha, sv, a, z, sz, t, st, p_outlier=0.1):
@@ -61,7 +61,7 @@ def wienerRL_like(x, v, alpha,dual_alpha, sv, a, z, sz, t, st, p_outlier=0.1):
     wp = wiener_params
 
     response = x['response'].values.astype(int)
-    q = np.array([x['q'].iloc[0],x['q'].iloc[0]])
+    q = np.array([q_up,q_low])
     feedback = x['feedback'].values
     split_by = x['split_by'].values
     unique = np.unique(split_by).shape[0]
