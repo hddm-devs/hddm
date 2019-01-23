e68e5fc0e55b4251c4e451c408297a7809844f60
madslupe <madslupe@gmail.com>
2019-01-23 11:25:44 -0500

    Update hddm_rl.py

--- hddm/models/hddm_rl.py
+++ hddm/models/hddm_rl.py
@@ -68,5 +68,5 @@ def wienerRL_like(x, v, alpha,dual_alpha, sv, a, z, sz, t, st, p_outlier=0.1):
     # could use something like the line below to avoid sending exp_up and exp_low as arrays. want to access the different values of 
     # of by u (below), but not using for-loop.
     #u, split_by = np.unique(x['split_by'].values, return_inverse=True)
-    return wiener_like_rlddm(x['rt'].values, response,feedback,q,split_by,unique,alpha,dual_alpha,v,sv, a, z, sz, t, st, p_outlier=p_outlier,uncertainty, **wp)
+    return wiener_like_rlddm(x['rt'].values, response,feedback,q,split_by,unique,alpha,dual_alpha,v,sv, a, z, sz, t, st,uncertainty, p_outlier=p_outlier, **wp)
 WienerRL = stochastic_from_dist('wienerRL', wienerRL_like)
