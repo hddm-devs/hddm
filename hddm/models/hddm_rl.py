57a907c5a5f69f12856b986cc7db2b4760dd6cfe
madslupe <madslupe@gmail.com>
2019-01-26 10:14:58 -0500

    Update hddm_rl.py

--- hddm/models/hddm_rl.py
+++ hddm/models/hddm_rl.py
@@ -16,10 +16,9 @@ class HDDMrl(HDDM):
     """HDDM model that can be used for two-armed bandit tasks.
 
     """
-    def __init__(self,q=0.5,uncertainty=0,*args, **kwargs):
-        #self.uncertainty = kwargs.pop('uncertainty', False)
-        self.uncertainty = uncertainty
-        self.q = q
+    def __init__(self,*args, **kwargs): #q=0.5,uncertainty=0,*args, **kwargs):
+        #self.uncertainty = uncertainty
+        #self.q = q
         self.alpha = kwargs.pop('alpha', True)
         self.dual_alpha = kwargs.pop('dual_alpha', False)
         self.wfpt_rl_class = WienerRL
@@ -37,22 +36,22 @@ class HDDMrl(HDDM):
                                                                     std_lower=1e-10,
                                                                     std_upper=10, 
                                                                     std_value=.1))
-        if self.uncertainty:
-            knodes.update(self._create_family_normal('uncertainty',
-                                                                    value=0,
-                                                                    g_mu=0.2,
-                                                                    g_tau=3**-2,
-                                                                    std_lower=1e-10,
-                                                                    std_upper=10, 
-                                                                    std_value=.1))
-        if self.q:
-            knodes.update(self._create_family_normal('q',
-                                                                    value=0,
-                                                                    g_mu=0.2,
-                                                                    g_tau=3**-2,
-                                                                    std_lower=1e-10,
-                                                                    std_upper=10, 
-                                                                    std_value=.1))
+        #if self.uncertainty:
+        #    knodes.update(self._create_family_normal('uncertainty',
+        #                                                            value=0,
+        #                                                            g_mu=0.2,
+        #                                                            g_tau=3**-2,
+        #                                                            std_lower=1e-10,
+        #                                                            std_upper=10, 
+        #                                                            std_value=.1))
+        #if self.q:
+        #    knodes.update(self._create_family_normal('q',
+        #                                                            value=0,
+        #                                                            g_mu=0.2,
+        #                                                            g_tau=3**-2,
+        #                                                            std_lower=1e-10,
+        #                                                            std_upper=10, 
+        #                                                            std_value=.1))
         return knodes
 
     def _create_wfpt_parents_dict(self, knodes):
@@ -61,8 +60,8 @@ class HDDMrl(HDDM):
         #wfpt_parents['uncertainty'] = self.uncertainty
         wfpt_parents['alpha'] = knodes['alpha_bottom']
         wfpt_parents['dual_alpha'] = knodes['dual_alpha_bottom'] if 'dual_alpha' in self.include else 0
-        wfpt_parents['uncertainty'] = knodes['uncertainty_bottom'] if 'dual_alpha' in self.include else self.uncertainty
-        wfpt_parents['q'] = knodes['q_bottom'] if 'q' in self.include else self.q
+        #wfpt_parents['uncertainty'] = knodes['uncertainty_bottom'] if 'dual_alpha' in self.include else self.uncertainty
+        #wfpt_parents['q'] = knodes['q_bottom'] if 'q' in self.include else self.q
         
         return wfpt_parents
 
@@ -80,9 +79,9 @@ def wienerRL_like(x, v, alpha,dual_alpha, sv, a, z, sz, t, st,q=0.5,uncertainty=
                          'simps_err':1e-3,
                          'w_outlier': 0.1}
     sum_logp = 0
-    print(uncertainty)
-    print(p_outlier)
-    print(q)
+    #print(uncertainty)
+    #print(p_outlier)
+    #print(q)
     wp = wiener_params
     #uncertainty = x['uncertainty'].iloc[0]
     response = x['response'].values.astype(int)
