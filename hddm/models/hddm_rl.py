92adebd000e1e54dc80bb1f27f75285f207cb75c
madslupe <madslupe@gmail.com>
2019-01-26 20:51:04 -0500

    Update hddm_rl.py

--- hddm/models/hddm_rl.py
+++ hddm/models/hddm_rl.py
@@ -90,10 +90,10 @@ def wienerRL_like(x, v, alpha,dual_alpha, sv, a, z, sz, t, st,q=0.5,uncertainty=
         q = np.array([q,q])
         #print(q)
         feedback = y['feedback'].values
-        split_by = y['split_by'].values
-        print(split_by)
+        #split_by = y['split_by'].values
+        #print(split_by)
         #unique = np.array([np.unique(split_by)])
         print("v = %.2f alpha = %.2f dual_alpha = %.2f a = %.2f qup = %.2f qlow = %.2f uncertainty = %.2f t = %.2f z = %.2f sv = %.2f st = %.2f p_outlier = %.2f" % (v,alpha,dual_alpha,a,q[1],q[0],uncertainty,t,z,sv,st, p_outlier))
-        sum_logp += wiener_like_rlddm(x['rt'].values, response,feedback,q,split_by,alpha,dual_alpha,v,sv, a, z, sz, t, st,uncertainty, p_outlier=p_outlier, **wp)
+        sum_logp += wiener_like_rlddm(x['rt'].values, response,feedback,q,alpha,dual_alpha,v,sv, a, z, sz, t, st,uncertainty, p_outlier=p_outlier, **wp)
     return sum_logp
 WienerRL = stochastic_from_dist('wienerRL', wienerRL_like)
