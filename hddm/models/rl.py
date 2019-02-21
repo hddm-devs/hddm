18c08be77aa8a5e63dd9f40d73bfe95abacd5074
madslupe <madslupe@gmail.com>
2019-02-21 10:43:38 -0500

    Update rl.py

--- hddm/models/rl.py
+++ hddm/models/rl.py
@@ -13,11 +13,12 @@ from hddm.models import HDDM
 from wfpt import wiener_like_rl
 
 class Hrl(HDDM):
-    """HDDM model that can be used for two-armed bandit tasks.
+    """RL model that can be used to analyze data from two-armed bandit tasks.
 
     """
     def __init__(self, *args, **kwargs):
         self.alpha = kwargs.pop('alpha', True)
+        self.scaler = kwargs.pop('scaler', True)
         self.dual_alpha = kwargs.pop('dual_alpha', False)
         self.rl_class = RL
 
@@ -34,11 +35,20 @@ class Hrl(HDDM):
                                                                     std_lower=1e-10,
                                                                     std_upper=10, 
                                                                     std_value=.1))
+        if self.scaler:
+            knodes.update(self._create_family_normal('scaler',
+                                                        value=0,
+                                                        g_mu=0.2,
+                                                        g_tau=3**-2,
+                                                        std_lower=1e-10,
+                                                        std_upper=10, 
+                                                        std_value=.1))
         return knodes
 
     def _create_wfpt_parents_dict(self, knodes):
         wfpt_parents = super(Hrl, self)._create_wfpt_parents_dict(knodes)
 
+        wfpt_parents['scaler'] = knodes['scaler_bottom']
         wfpt_parents['alpha'] = knodes['alpha_bottom']
         wfpt_parents['dual_alpha'] = knodes['dual_alpha_bottom'] if 'dual_alpha' in self.include else 0
         
@@ -48,10 +58,10 @@ class Hrl(HDDM):
     def _create_wfpt_knode(self, knodes):
         wfpt_parents = self._create_wfpt_parents_dict(knodes)
         return Knode(self.rl_class, 'wfpt',
-                                   observed=True, col_name=['split_by','rew_up', 'rew_low', 'response', 'rt','exp_up','exp_low'],
+                                   observed=True, col_name=['split_by','feedback', 'response','q_init'],
                                    **wfpt_parents)
 
-def RL_like(x, v, alpha,dual_alpha, sv, a, z, sz, t, st, p_outlier=0.1):
+def RL_like(x, scaler, alpha,dual_alpha, p_outlier=0):
     
     wiener_params = {'err': 1e-4, 'n_st':2, 'n_sz':2,
                          'use_adaptive':1,
@@ -60,16 +70,14 @@ def RL_like(x, v, alpha,dual_alpha, sv, a, z, sz, t, st, p_outlier=0.1):
     sum_logp = 0
     wp = wiener_params
 
-    response = x['response'].values
-    exp_up = x['exp_up'].values
-    exp_low = x['exp_low'].values
-    rew_up = x['rew_up'].values
-    rew_low = x['rew_low'].values
+    response = x['response'].values.astype(int)
+    uncertainty = 0 #x['uncertainty'].iloc[0]
+    q = x['q_init'].iloc[0]
+    feedback = x['feedback'].values
     split_by = x['split_by'].values
-    unique = np.unique(split_by).shape[0]
     # could use something like the line below to avoid sending exp_up and exp_low as arrays. want to access the different values of 
     # of by u (below), but not using for-loop.
     #u, split_by = np.unique(x['split_by'].values, return_inverse=True)
-    return wiener_like_rl(response,rew_up,rew_low,exp_up,exp_low,split_by,unique,alpha,dual_alpha,v,sv, z, sz, t, st, p_outlier=p_outlier, **wp)
+    return wiener_like_rl(response,feedback,split_by,q,alpha,dual_alpha,scaler, p_outlier=p_outlier, **wp)
 RL = stochastic_from_dist('RL', RL_like)
 
