f7b6aa7282cc7ef58bf01be9d89c8d7a881a06df
madslupe <madslupe@gmail.com>
2019-02-21 11:44:28 -0500

    Update wfpt.pyx

--- src/wfpt.pyx
+++ src/wfpt.pyx
@@ -162,8 +162,8 @@ def wiener_like_rl(np.ndarray[double, ndim=1] x, np.ndarray[double, ndim=1] resp
                       np.ndarray[double, ndim=1] feedback,
                       np.ndarray[long, ndim=1] split_by,
                       double q,
-                      double alpha, double dual_alpha, double scaler,double z, long uncertainty,
-                      int n_st=10, int n_sz=10, bint use_adaptive=1, double simps_err=1e-8,
+                      double alpha, double dual_alpha, double v, double sv, double a, double z, double sz, double t,
+                      double st, long uncertainty, double err, int n_st=10, int n_sz=10, bint use_adaptive=1, double simps_err=1e-8,
                       double p_outlier=0, double w_outlier=0):
     cdef Py_ssize_t size = x.shape[0]
     cdef Py_ssize_t i
