c7dfbd850d6f5824ecf31b002a26b7d06731d4f6
madslupe <madslupe@gmail.com>
2019-02-22 16:22:17 -0500

    Update rl.py

--- hddm/models/rl.py
+++ hddm/models/rl.py
@@ -1,4 +1,5 @@
 
+
 """
 """
 
@@ -11,6 +12,7 @@ from kabuki.hierarchical import Knode
 from kabuki.utils import stochastic_from_dist
 from hddm.models import HDDM
 from wfpt import wiener_like_rl
+from collections import OrderedDict
 
 class Hrl(HDDM):
     """RL model that can be used to analyze data from two-armed bandit tasks.
@@ -18,15 +20,16 @@ class Hrl(HDDM):
     """
     def __init__(self, *args, **kwargs):
         self.alpha = kwargs.pop('alpha', True)
-        #self.scaler = kwargs.pop('scaler', True)
         self.dual_alpha = kwargs.pop('dual_alpha', False)
         self.rl_class = RL
 
         super(Hrl, self).__init__(*args, **kwargs)
 
     def _create_stochastic_knodes(self, include):
-        print(include)
-        #self.include = set(['alpha', 'v'])
+        if 'dual_alpha' in self.include:
+            include = set(['v','dual_alpha'])
+        else:
+            include = set(['v'])
         knodes = super(Hrl, self)._create_stochastic_knodes(include)
         if self.alpha:
             # Add learning rate parameter
@@ -37,20 +40,12 @@ class Hrl(HDDM):
                                                                     std_lower=1e-10,
                                                                     std_upper=10, 
                                                                     std_value=.1))
-        #if self.scaler:
-        #    knodes.update(self._create_family_normal('scaler',
-        #                                                value=0,
-        #                                                g_mu=0.2,
-        #                                                g_tau=3**-2,
-        #                                                std_lower=1e-10,
-        #                                                std_upper=10, 
-        #                                                std_value=.1))
         return knodes
 
     def _create_wfpt_parents_dict(self, knodes):
-        wfpt_parents = super(Hrl, self)._create_wfpt_parents_dict(knodes)
+        wfpt_parents =  OrderedDict() #super(Hrl, self)._create_wfpt_parents_dict(knodes)
 
-        #wfpt_parents['scaler'] = knodes['scaler_bottom']
+        wfpt_parents['v'] = knodes['v_bottom']
         wfpt_parents['alpha'] = knodes['alpha_bottom']
         wfpt_parents['dual_alpha'] = knodes['dual_alpha_bottom'] if 'dual_alpha' in self.include else 0
         
@@ -60,10 +55,10 @@ class Hrl(HDDM):
     def _create_wfpt_knode(self, knodes):
         wfpt_parents = self._create_wfpt_parents_dict(knodes)
         return Knode(self.rl_class, 'wfpt',
-                                   observed=True, col_name=['split_by','feedback', 'response','rt','q_init'],
+                                   observed=True, col_name=['split_by','feedback', 'response','q_init'],
                                    **wfpt_parents)
 
-def RL_like(x, v, alpha,dual_alpha, sv, a, z, sz, t, st ,p_outlier=0):
+def RL_like(x, v, alpha,dual_alpha,p_outlier=0):
     
     wiener_params = {'err': 1e-4, 'n_st':2, 'n_sz':2,
                          'use_adaptive':1,
@@ -73,13 +68,9 @@ def RL_like(x, v, alpha,dual_alpha, sv, a, z, sz, t, st ,p_outlier=0):
     wp = wiener_params
 
     response = x['response'].values.astype(int)
-    uncertainty = 0 #x['uncertainty'].iloc[0]
     q = x['q_init'].iloc[0]
     feedback = x['feedback'].values
     split_by = x['split_by'].values
-    # could use something like the line below to avoid sending exp_up and exp_low as arrays. want to access the different values of 
-    # of by u (below), but not using for-loop.
-    #u, split_by = np.unique(x['split_by'].values, return_inverse=True)
-    return wiener_like_rl(response,feedback,split_by,q,alpha,dual_alpha,v,sv, a, z, sz, t, st,uncertainty, p_outlier=p_outlier, **wp)
+    return wiener_like_rl(response,feedback,split_by,q,alpha,dual_alpha,v,0, 0, 0.5, 0, 0, 0,p_outlier=p_outlier, **wp)
 RL = stochastic_from_dist('RL', RL_like)
 
