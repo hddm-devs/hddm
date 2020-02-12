1e78aade61d67d400a32033352a4c947471fa0db
madslupe <madslupe@gmail.com>
2020-02-12 09:51:08 +0100

    bump version to 0.7.5 (#55)
    * reset q-values
    
    * loc not ix
    
    * bump version to 0.7.5
    
    * s
    
    * Revert "s"
    
    This reverts commit 229f3103b657179410405b1374b075e866a5f9e2.
    
    * Revert "bump version to 0.7.5"
    
    This reverts commit 01c332c0e0379c60623a75fa7cebf9be72df04bf.
    
    * bump version to 0.7.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import numpy as np
 
 setup(
     name='HDDM',
-    version='0.7.4',
+    version='0.7.5',
     author='Thomas V. Wiecki, Imri Sofer, Michael J. Frank, Mads Lund Pedersen',
     author_email='thomas.wiecki@gmail.com',
     url='http://github.com/hddm-devs/hddm',
