01bde15e787073a3c04f2f9fe109ebb8c9655f9f
Alexander Fengler <alexanderfengler@gmx.de>
2021-07-29 19:51:27 -0400

    first step towards making tensorflow optional

--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ setup(
     package_data={'hddm':['examples/*.csv', 'examples/*.conf', 'keras_models/*.h5', 'cnn_models/*/*', 'simulators/*']},
     scripts=['scripts/hddm_demo.py'],
     description='HDDM is a python module that implements Hierarchical Bayesian estimation of Drift Diffusion Models.',
-    install_requires=['NumPy >=1.6.0', 'SciPy >= 0.6.0', 'kabuki >= 0.6.0', 'PyMC >= 2.3.3', 'pandas >= 0.12.0', 'patsy', 'seaborn >= 0.11.0', 'statsmodels >= 0.12.0', 'tensorflow >= 1.15', 'h5py == 2.10.0', 'tqdm >= 4.1.0', 'scikit-learn >= 0.24'],
-    setup_requires=['NumPy >=1.6.0', 'SciPy >= 0.6.0', 'kabuki >= 0.6.0', 'PyMC >= 2.3.3', 'pandas >= 0.12.0', 'patsy', 'seaborn >= 0.11.0', 'statsmodels >= 0.12.0', 'tensorflow >= 1.15', 'h5py == 2.10.0', 'tqdm >= 4.1.0', 'scikit-learn >= 0.24'],
+    install_requires=['NumPy >=1.6.0', 'SciPy >= 0.6.0', 'kabuki >= 0.6.0', 'PyMC >= 2.3.3', 'pandas >= 0.12.0', 'patsy', 'seaborn >= 0.11.0', 'statsmodels >= 0.12.0', 'h5py < 3.0.0', 'tqdm >= 4.1.0', 'scikit-learn >= 0.24'],
+    setup_requires=['NumPy >=1.6.0', 'SciPy >= 0.6.0', 'kabuki >= 0.6.0', 'PyMC >= 2.3.3', 'pandas >= 0.12.0', 'patsy', 'seaborn >= 0.11.0', 'statsmodels >= 0.12.0', 'h5py < 3.0.0', 'tqdm >= 4.1.0', 'scikit-learn >= 0.24'],
     include_dirs = [np.get_include()],
     classifiers=[
                 'Development Status :: 5 - Production/Stable',
