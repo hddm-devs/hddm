cb671dbea36d6d146032b7b74c32ac73f67598b9
Alexander Fengler <alexanderfengler@gmx.de>
2021-11-02 21:40:05 -0400

    some changes to make unit tests respect newer panda versions

--- hddm/simulators/boundary_functions.py
+++ hddm/simulators/boundary_functions.py
@@ -9,23 +9,51 @@ import numpy as np
 
 # Constant: (multiplicative)
 def constant(t=0):
+    """constant boundary"""
     return 1
 
 
 # Angle (additive)
 def angle(t=1, theta=1):
+    """angle boundary
+    
+        :Arguments:
+            t: np.array or float <default = 1>
+                Time/s (with arbitrary measure, but in HDDM it is used as seconds) at which to evaluate the bound.
+            theta: float <default = 1>
+                Angle of the bound in radians.
+            
+    """
     return np.multiply(t, (-np.sin(theta) / np.cos(theta)))
 
 
 # Generalized logistic bound (additive)
 def generalized_logistic_bnd(t=1, B=2.0, M=3.0, v=0.5):
+    """generalized logistic bound
+    
+        :Arguments:
+            t: np.array or float <default = 1>
+                Time/s (with arbitrary measure, but in HDDM it is used as seconds) at which to evaluate the bound.
+            B: float <default = 2.0>
+            M: float <default = 3.0>
+            v: float <default = 0.5>
+          
+    """
     return 1 - (1 / np.power(1 + np.exp(-B * (t - M)), 1 / v))
 
 
 # Weibull survival fun (multiplicative)
 def weibull_cdf(t=1, alpha=1, beta=1):
+    """generalized logistic bound
+    
+        :Arguments:
+            t: np.array or float <default = 1>
+                Time/s (with arbitrary measure, but in HDDM it is used as seconds) at which to evaluate the bound.
+            alpha: float <default = 1.0>
+            beta: float <default = 1.0>
+          
+    """
     return np.exp(-np.power(np.divide(t, beta), alpha))
-
 
 # # Gamma shape: (additive)
 # def gamma_bnd(t = 1,
