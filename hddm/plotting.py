7e4e98d1f8eb098c40151f3c2efb4a41d4458b31
Alexander Fengler <alexanderfengler@gmx.de>
2021-11-16 21:28:50 -0500

    fixed posterior pair plot

--- hddm/plotting.py
+++ hddm/plotting.py
@@ -1646,14 +1646,14 @@ def posterior_pair_plot(
         hddm_model=hddm_model, model_ground_truth=model_ground_truth
     )
 
+    print(data)
+
     sns.set()
     sns.set_theme(style = 'ticks', rc = {"axes.spines.right": False, 
                                          "axes.spines.top": False})
     sns.set_context("notebook", font_scale = font_scale)
     plot_cnt = 0
     for c_tmp in data.keys():
-        print('data[c_tmp].keys()')
-        print(data[c_tmp].keys())
         for s_tmp in data[c_tmp].keys():
             # Turn traces into dataframe:
             # Get all ground truths
@@ -1683,7 +1683,8 @@ def posterior_pair_plot(
             data[c_tmp][s_tmp]["traces"] = pd.DataFrame(
                 data[c_tmp][s_tmp]["traces"], columns=data[c_tmp][s_tmp]["trace_names"]
             )
-            print(data[c_tmp][s_tmp])
+
+
             g = sns.PairGrid(
                 data[c_tmp][s_tmp]["traces"].sample(n_subsample),
                 height=height,
