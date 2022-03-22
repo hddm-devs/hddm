915f648737d795874c2fb0c7aff1d0c5315fcf6d
Alexander Fengler <alexanderfengler@gmx.de>
2022-03-22 12:36:26 -0400

    take out a print statement

--- hddm/models/base.py
+++ hddm/models/base.py
@@ -1364,11 +1364,11 @@ class HDDMBase(AccumulatorModel):
         self.p_outlier = p_outlier
 
         # set cdf_range
-        print('data["rt"]')
-        print(data["rt"])
-        print(np.abs(data["rt"]))
-        print(max(np.abs(data["rt"])))
-        
+        print('data')
+        print(data)
+        #print(np.abs(data["rt"]))
+        #print(max(np.abs(data["rt"])))
+
         cdf_bound = max(np.abs(data["rt"])) + 1
         self.cdf_range = (-cdf_bound, cdf_bound)
 
