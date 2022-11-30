effaa5b5a928d15c8f6c1f08386c4921e481b1a1
Alexander Fengler <alexanderfengler@gmx.de>
2022-11-29 19:41:48 -0500

    missing argument

--- hddm/plotting.py
+++ hddm/plotting.py
@@ -1250,6 +1250,7 @@ def _plot_func_model(
     if add_trajectories:
         _add_trajectories(axis = axis,
                           sample = node_data_full,
+                          tmp_model = tmp_model,
                           t_s = t_s,
                           **kwargs)
 
@@ -1257,6 +1258,7 @@ def _add_trajectories(
         axis = None,
         sample = None,
         t_s = None,
+        tmp_model = None,
         n_trajectories = 10,
         supplied_trajectory = None,
         maxid_trajectories = 1, #useful for gifs
