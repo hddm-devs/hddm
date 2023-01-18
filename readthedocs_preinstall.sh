f2e9c65665f4324c9662f0df6527e0b670606ad8
Alexander Fengler <alexanderfengler@gmx.de>
2023-01-19 00:23:18 +0100

    new attmept at install gfortran on readthedocs

--- readthedocs_preinstall.sh
+++ readthedocs_preinstall.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 pip install numpy>=1.20
 pip install tqdm
+pip install seaborn>0.11
+pip install scikit-learn>=0.24
+pip install cloudpickle>=2.0.0
+pip install arviz >= 0.11
+
 echo $(gfortran --version)
 echo $(gcc --version)
